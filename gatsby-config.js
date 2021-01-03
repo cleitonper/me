5c4e3ed27a108866837cab61d7977e0d829055ef
Cleiton da Silva <cleiton.spereira@live.com>
2021-01-02 21:35:14 -0300

    🐛 don't precache home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,7 +77,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-offline',
       options: {
-        precachePages: ['/', '/blog/*'],
+        precachePages: ['/blog/*'],
       },
     },
     {
