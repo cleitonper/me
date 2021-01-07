788747caae7e380aa1f20f5b535c9cf058cd111c
Cleiton da Silva <cleiton.spereira@live.com>
2021-01-07 17:23:37 -0300

    ✅ update tests

--- src/components/RecentWork/component.spec.tsx
+++ src/components/RecentWork/component.spec.tsx
@@ -5,10 +5,26 @@ import { RecentWork } from '.';
 
 describe('Component: RecentWork', () => {
   it('should render a list of jobs', () => {
+    const image = {
+      childImageSharp: {
+        fluid: {
+          aspectRatio: 1,
+          src: 'fake.jpg',
+          srcSet:
+            ` fake-480w.jpg 480w,
+              fake-800w.jpg 800w`,
+          sizes:
+            `(max-width: 600px) 480px,
+            800px`,
+        }
+      }
+    };
+
     const jobs: Job[] = [
-      { image: 'job-01', link: '#', description: 'Job 01', tags: [{ background: 'a', foreground: 'b', name: 'c' }] },
-      { image: 'job-02', link: '#', description: 'Job 02', tags: [{ background: 'a', foreground: 'b', name: 'c' }] },
+      { image: image, link: '#', description: 'Job 01', tags: [{ background: 'a', foreground: 'b', name: 'c' }] },
+      { image: image, link: '#', description: 'Job 02', tags: [{ background: 'a', foreground: 'b', name: 'c' }] },
     ];
+
     const { getAllByTestId } = render(<RecentWork jobs={jobs} />);
     expect(getAllByTestId('job')).toHaveLength(jobs.length);
   });
