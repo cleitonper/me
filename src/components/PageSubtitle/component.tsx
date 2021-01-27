74304c763a56bd057d0bec9114cd723e07ce32f9
Cleiton da Silva <cleiton.spereira@live.com>
2021-01-26 21:55:44 -0300

    💄 update typography

--- src/components/PageSubtitle/component.tsx
+++ src/components/PageSubtitle/component.tsx
@@ -6,10 +6,20 @@ const Subtitle = styled('h2')`
   margin: 0px auto 48px;
   text-align: center;
   font-weight: 300;
-  font-size: 1.95rem;
-  line-height: 1.25em;
   max-width: 1200px;
   width: 100%;
+
+  @media (max-width: 767px) {
+    font-size: 1.40rem;
+    line-height: 1.45em;
+    letter-spacing: 0.025em;
+  }
+
+  @media (min-width: 768px) {
+    font-size: 1.95rem;
+    line-height: 1.25em;
+    letter-spacing: 0.010em;
+  }
 `;
 
 const PageSubtitle: FunctionComponent = ({ children }) => (
